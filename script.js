e3e39e3ceff6effe7aec0dc55c15613be3161abe
Jyoti Gupta <jyotigupta@git.training>
2023-12-29 16:21:54 +0530

    modifies script.js

--- script.js
+++ script.js
@@ -10,11 +10,18 @@ document.querySelector('.score').textContent = 23;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);*/
 
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
 
-  if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number!';
+  //   if (!guess) {
+  //     document.querySelector('.message').textContent = '⛔ No number!';
+  //   }
+  if (guess === 0) {
+    document.querySelector('.message').textContent =
+      'Please enter number between 1 and 20.';
   }
 });
